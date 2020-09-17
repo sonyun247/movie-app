41568a6e9c7da34d57053ee71f9ee50ae7045e0e
LEE Seungje <38553239+sonyun247@users.noreply.github.com>
2020-09-17 16:45:30 +0900

    learn fundamentals
    jsx, props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+const list = [
+  { id: 1, name: "A", country: "korea" },
+  { id: 2, name: "B", country: "america" },
+  { id: 3, name: "C", country: "japan" },
+];
+
+function SayHi({ name, country }) {
+  return (
+    <div>
+      <span>Hello, {name}</span> <h3>Your country is {country}</h3>
+    </div>
+  );
+}
+
+function getPerson(person) {
+  return <SayHi key={person.id} name={person.name} country={person.country} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test</h1>
+      {list.map(getPerson)}
     </div>
   );
 }
